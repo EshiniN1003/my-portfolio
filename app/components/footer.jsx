8279c48458ee20c58518d30d8438baf99bb4322b
EshiniN1003 <eshini.nethmini1003@gmail.com>
2024-10-27 13:52:28 +0530

    new

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © GitHub Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/eshi-nethmini/" className="text-primary-icon">Eshini Nethmini</Link>
+            © GitHub Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/eshini-nethmini/" className="text-primary-icon">Eshini Nethmini</Link>
           </p>
           <div className="flex items-center gap-5">
             <Link
--- data/user-data.js
+++ data/user-data.js
@@ -1,7 +1,7 @@
 export const userData = {
   githubUser: 'EshiniN1003', 
   github: 'https://github.com/EshiniN1003', 
-  linkedIn: 'https://www.linkedin.com/in/eshi-nethmini/', 
+  linkedIn: 'https://www.linkedin.com/in/eshini-nethmini/', 
   resume: "https://drive.google.com/file/d/15ZeVOKSBcTZ508INVZktKEHvM20M25nr/view?usp=sharing", 
   skills: ['React', 'NextJS',  'MySql',  'Postgres', 'Python','HTML','css'], 
   timezone: '+5:30'
