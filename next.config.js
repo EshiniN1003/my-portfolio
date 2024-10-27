fc28e202f066761e5b8a204062bb87163126af1f
EshiniN1003 <eshini.nethmini1003@gmail.com>
2024-10-27 22:56:05 +0530

    new

--- next.config.js
+++ next.config.js
@@ -1,30 +1,30 @@
-const path = require('path')
- 
-module.exports = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  images: {
-    domains: ['media.dev.to', 'avatars.githubusercontent.com', 'github-readme-stats.vercel.app', 'github-readme-streak-stats.herokuapp.com', 'stardev.io', 'github-profile-summary-cards.vercel.app', 'github-profile-trophy.vercel.app'],
-    dangerouslyAllowSVG: true,
-  }
-}
+const path = require('path');
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
   output: 'export',
- 
-  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-  // trailingSlash: true,
- 
-  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-  // skipTrailingSlashRedirect: true,
- 
-  // Optional: Change the output directory `out` -> `dist`
-  // distDir: 'dist',
-}
- 
-module.exports = nextConfig
+  basePath: '/my-portfolio', // Replace 'my-portfolio' with your actual repository name
+  assetPrefix: '/my-portfolio',
+  trailingSlash: true, // Optional for directory structure
+  
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  
+  images: {
+    domains: [
+      'media.dev.to', 
+      'avatars.githubusercontent.com', 
+      'github-readme-stats.vercel.app', 
+      'github-readme-streak-stats.herokuapp.com', 
+      'stardev.io', 
+      'github-profile-summary-cards.vercel.app', 
+      'github-profile-trophy.vercel.app'
+    ],
+    dangerouslyAllowSVG: true,
+  },
+};
 
-
+module.exports = nextConfig;
