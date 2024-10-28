c7887faffa1ef8dfb57113d49c05705a216ae6ea
EshiniN1003 <eshini.nethmini1003@gmail.com>
2024-10-28 22:02:13 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const path = require('path');
  */
 const nextConfig = {
   output: 'export',
-  basePath: '/my-portfolio', // Replace with your actual repository name if needed
-  assetPrefix: '/my-portfolio', // Set asset prefix for assets
+  basePath: '.', // Replace with your actual repository name if needed
+  assetPrefix: '.', // Set asset prefix for assets
   trailingSlash: true, // Optional for directory structure
   
   sassOptions: {
